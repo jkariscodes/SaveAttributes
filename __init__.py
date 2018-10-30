8f664c111d46f09d365fb9d304162c0f5dfeb813
HEMPIRE <joehene@gmail.com>
2018-10-30 04:27:48 +0300

    initial commit

---
+++ __init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ SaveAttributes
+                                 A QGIS plugin
+ This plugin saves attributes
+                             -------------------
+        begin                : 2018-10-30
+        copyright            : (C) 2018 by Joseph Kariuki
+        email                : joehene@gmail.com
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load SaveAttributes class from file SaveAttributes.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgisInterface
+    """
+    #
+    from .save_attributes import SaveAttributes
+    return SaveAttributes(iface)
